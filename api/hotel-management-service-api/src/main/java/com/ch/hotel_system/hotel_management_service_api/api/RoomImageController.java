3b2ce7694bc76b1ee4e835cd0def6d8893d9d5a8
Harendra Samarawickrama <samarawickramaharendra@gmail.com>
2025-07-26 11:51:28 +0530

    Created controllers

---
+++ api/hotel-management-service-api/src/main/java/com/ch/hotel_system/hotel_management_service_api/api/RoomImageController.java
@@ -0,0 +1,9 @@
+package com.ch.hotel_system.hotel_management_service_api.api;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/hotel-management/api/v1/images")
+public class RoomImageController {
+}
